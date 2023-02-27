096bdea1b068d2df55a34e5d9493cbaf3e6e11dd
NesTerLoN392 <tcncarlos392@gmail.com>
2023-02-26 19:43:36 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className='burbujas'>
         <div className='burbuja'></div>
         <div className='burbuja'></div>
-        <div className='burbuja'></div>
+        <div className='burbuja'> <div className='container-icon'><a className='span' href="https://www.linkedin.com/in/carlos-mu%C3%B1oz-cuellar-programador/" target={'_blank'}><i className='bx bxl-linkedin-square bx-md'></i></a></div><div>Contactame!</div></div>
         <div className='burbuja'></div>
         <div className='burbuja'></div>
       </div>
